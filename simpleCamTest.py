b52860b4a7377ee1cdf89f2e194f5e97eaf974ec
Lars Huring <lars.huring@gmail.com>
2018-07-03 12:16:36 +0200

    Updated

--- simpleCamTest.py
+++ simpleCamTest.py
@@ -6,10 +6,10 @@ cap.set(4,480)
 while(True):
 	ret, frame = cap.read()
 	frame = cv2.flip(frame, -1)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	cv2.imshow('frame', frame)
-	cv2.imshow('gray', gray)
+	#cv2.imshow('gray', gray)
 
 	k = cv2.waitKey(30) & 0xff
 	if k == 27:
